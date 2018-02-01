169448742983e06ec2b333a07c8310d254a093b0
etudiant 2 <williambalea@gmail.com>
2018-01-31 20:42:49 -0500

    WB: somekeyhash.cpp modified

--- TP1/src/SomeKeyHash.cpp
+++ TP1/src/SomeKeyHash.cpp
@@ -8,6 +8,12 @@ int SomeKeyHash::operator()(const std::string& s) const {
   return hash(s.c_str()) % modu;
 }
 
-unsigned long SomeKeyHash::hash(const char *str) const {
-  return 1;
+unsigned long SomeKeyHash::hash(const unsigned char *str) const {
+	unsigned long hash = 5381;
+	int c;
+
+	while (c = *str++)
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+	return hash;
 }
