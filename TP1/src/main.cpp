0f4a17248d26e78217ae478760a8816780e05d9a
etudiant 2 <williambalea@gmail.com>
2018-01-31 21:14:41 -0500

    WB: added lireFichier to main.cpp

--- TP1/src/main.cpp
+++ TP1/src/main.cpp
@@ -11,30 +11,52 @@
 using namespace std;
 
 /* */
-int main(int argc, char** argv) {
 
-  //utilisation normale
-  HashMap hmap;
-  hmap.put("v1",1);
-  hmap.put("v3",1);
-  hmap.put("v11",1);
-  hmap.put("v13",13);
-  
-  int value;
-  bool res = hmap.get("v13", value);
-  if (res)
-    cout << value << endl;
-  
-  hmap.remove("v1");
-  res = hmap.get("v1", value);
-  if (res)
-    cout << "ERROR: " << value << endl;
+void readFile(string NomFichier){
+	ifstream file;
+	file.open(NomFichier);
+	string word;
+	char x;
+	word.clear();
 
-  std::vector<std::string> keys=hmap.getKeys();
-  for(int i=0;i<keys.size();i++){
-    cout << keys[i] << endl;
-  }
-  
-  return 0;
+	while (file >> word){
+		x = file.get();
+
+		while (x != ' '){
+			word = word + x;
+			x = file.get();
+		}
+
+		cout << word << endl;
+		word.clear();
+	}
+}
+
+int main(int argc, char** argv, string nomFichier) {
+	readFile(nomFichier);
+
+	//utilisation normale
+	HashMap hmap;
+	hmap.put("v1", 1);
+	hmap.put("v3", 1);
+	hmap.put("v11", 1);
+	hmap.put("v13", 13);
+
+	int value;
+	bool res = hmap.get("v13", value);
+	if (res)
+		cout << value << endl;
+
+	hmap.remove("v1");
+	res = hmap.get("v1", value);
+	if (res)
+		cout << "ERROR: " << value << endl;
+
+	std::vector<std::string> keys = hmap.getKeys();
+	for (int i = 0; i < keys.size(); i++) {
+		cout << keys[i] << endl;
+	}
+
+	return 0;
 }
 
